7a0bc4c3c4efb2389732353169fa9861a44e0dff
stephen-jones7 <stephen.jones7@protonmail.com>
2020-05-16 00:05:38 -0500

    1 file modified updated the import style.css call

--- index.js
+++ index.js
@@ -5,14 +5,37 @@ import React from "react";
 
 //var ReactDOM = require("react-dom");
 import ReactDOM from "react-dom";
+import './style.css';
+
 
 //ReactDOM.render(what to ShadowRoot, where to show it);
 // div is needed if you need to show more than one Element
 // at a timed
+
+
+const d = new Date();
+const time = d.getHours()
+const strMsg = "";
+const customStyle = {
+  color: "red"
+}
+
+if (time>0 && time < 12) {
+  strMsg = "Good Morning";
+  customStyle.color = "red";
+} else if (time >12 && time < 18) {
+  strMsg = "Good Afternoon";
+  customStyle.color = "green";
+} else {
+  strMsg = "Good Evening";
+  customStyle.color = "blue";
+};
+
+
 ReactDOM.render(
   <div>
-    <h1>Hello world.</h1>
-    <p>This is a new paragraph.!</p>
+    <h1 className="heading" style = {customStyle}>{strMsg}</h1>
+    
   </div>,
   document.getElementById("root")
 );
