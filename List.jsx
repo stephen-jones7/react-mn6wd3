f7c18a70148fe41ebbac3a1f9812145312440c94
stephen-jones7 <stephen.jones7@protonmail.com>
2020-05-16 00:48:28 -0500

    2 files modified, 3 added added component files .jsx

---
+++ List.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function List() {
+  return [
+    
+    <ul>
+      <li>Pizza</li>
+      <li>Tea</li>
+      <li>Ice Cream</li>
+    </ul>
+    
+  ];
+
+}
+
+export default List;
+
